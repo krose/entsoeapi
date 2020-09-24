554808d3e398c3717250d0ce05f0a24b830dffc7
Kenneth Rose <kennethrose82@gmail.com>
2020-09-24 14:59:09 +0200

    New function for day ahead scheduled flow.

--- R/en_transmission_forecasted_capacity.R
+++ R/en_transmission_forecasted_capacity.R
@@ -197,3 +197,63 @@ en_transmission_day_ahead_prices_eic <- function(){
 
   trans_df
 }
+
+
+
+#' Get Day ahead prices from Entsoe
+#'
+#' @param eic_in Energy Identification Code
+#' @param eic_out Energy Identification Code
+#' @param period_start POSIXct
+#' @param period_end POSIXct
+#' @param security_token Security token
+#'
+#' @export
+#'
+#' @examples
+#'
+#'  library(tidyverse)
+#'  library(entsoeapi)
+#'
+#'  en_transmission_day_ahead_commercial_schedules(eic_in = "10YCZ-CEPS-----N", eic_out = "10YSK-SEPS-----K", period_start = lubridate::ymd("2019-11-01", tz = "CET"), period_end = lubridate::ymd("2019-12-01", tz = "CET"))
+#'  en_transmission_day_ahead_commercial_schedules(eic_in = "10YDK-1--------W", eic_out = "10Y1001A1001A82H", period_start = lubridate::ymd("2019-11-01", tz = "CET"), period_end = lubridate::ymd("2019-12-01", tz = "CET"))
+#'
+en_transmission_day_ahead_commercial_schedules <- function(eic_in, eic_out, period_start, period_end, security_token = NULL){
+
+  period_start <- url_posixct_format(period_start)
+  period_end <- url_posixct_format(period_end)
+
+  if(is.null(security_token)){
+    security_token <- Sys.getenv("ENTSOE_PAT")
+  }
+
+  if(length(eic_in) > 1 | length(eic_out) > 1){
+    stop("This wrapper only supports one EIC per request.")
+  }
+
+  url <- paste0(
+    "https://transparency.entsoe.eu/api",
+    "?documentType=A09",
+    "&contract_MarketAgreement.Type=A01",
+    "&in_Domain=", eic_in,
+    "&out_Domain=", eic_out,
+    "&periodStart=",period_start,
+    "&periodEnd=", period_end,
+    "&securityToken=", security_token
+  )
+
+  en_cont <- api_req(url)
+
+  en_cont <- xml2::as_list(en_cont)
+
+  en_cont <- tm_quantity_helper(en_cont)
+  en_cont$quantity_measure_unit_name <- "MAW"
+  en_cont$in_domain_mrid <- eic_in
+  en_cont$out_domain_mrid <- eic_out
+
+  en_cont <- en_cont[, c("in_domain_mrid", "out_domain_mrid", "quantity_measure_unit_name", "dt", "quantity")]
+
+  en_cont
+}
+
+
