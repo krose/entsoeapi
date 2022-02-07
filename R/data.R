fdb476b5dd3a517509a20f2dfdcff3f38d82f003
Sándor Budai <sbudai.ga@gmail.com>
2022-02-07 18:18:23 +0100

    basic documentation added to ...TypeList tables

---
+++ R/data.R
@@ -0,0 +1,115 @@
+#' StandardAllocationModeTypeList
+#'
+"StandardAllocationModeTypeList"
+
+#' StandardAssetTypeList
+#'
+"StandardAssetTypeList"
+
+#' StandardAuctionTypeList
+#'
+"StandardAuctionTypeList"
+
+#' StandardBusinessTypeList
+#'
+"StandardBusinessTypeList"
+
+#' StandardCategoryTypeList
+#'
+"StandardCategoryTypeList"
+
+#' StandardClassificationTypeList
+#'
+"StandardClassificationTypeList"
+
+#' StandardCodingSchemeTypeList
+#'
+"StandardCodingSchemeTypeList"
+
+#' StandardContractTypeList
+#'
+"StandardContractTypeList"
+
+#' StandardCurrencyTypeList
+#'
+"StandardCurrencyTypeList"
+
+#' StandardCurveTypeList
+#'
+"StandardCurveTypeList"
+
+#' StandardDirectionTypeList
+#'
+"StandardDirectionTypeList"
+
+#' StandardDocumentTypeList
+#'
+"StandardDocumentTypeList"
+
+#' StandardEicTypeList
+#'
+"StandardEicTypeList"
+
+#' StandardEnergyProductTypeList
+#'
+"StandardEnergyProductTypeList"
+
+#' StandardHVDCModeTypeList
+#'
+"StandardHVDCModeTypeList"
+
+#' StandardIndicatorTypeList
+#'
+"StandardIndicatorTypeList"
+
+#' StandardObjectAggregationTypeList
+#'
+"StandardObjectAggregationTypeList"
+
+#' StandardPaymentTermsTypeList
+#'
+"StandardPaymentTermsTypeList"
+
+#' StandardPriceCategoryTypeList
+#'
+"StandardPriceCategoryTypeList"
+
+#' StandardPriceDirectionTypeList
+#'
+"StandardPriceDirectionTypeList"
+
+#' StandardProcessTypeList
+#'
+"StandardProcessTypeList"
+
+#' StandardQualityTypeList
+#'
+"StandardQualityTypeList"
+
+#' StandardReasonCodeTypeList
+#'
+"StandardReasonCodeTypeList"
+
+#' StandardRightsTypeList
+#'
+"StandardRightsTypeList"
+
+#' StandardRoleTypeList
+#'
+"StandardRoleTypeList"
+
+#' StandardStatusTypeList
+#'
+"StandardStatusTypeList"
+
+#' StandardTarifTypeTypeList
+#'
+"StandardTarifTypeTypeList"
+
+#' StandardUnitOfMeasureTypeList
+#'
+"StandardUnitOfMeasureTypeList"
+
+#' StandardUnitSymbolTypeList
+#'
+"StandardUnitSymbolTypeList"
