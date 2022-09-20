c53b18915aecc1eae14757c7dd3279e6330c671b
Kenneth Rose <kennethrose82@gmail.com>
2022-09-20 11:58:42 +0200

    I moved the function en_transmission_day_ahead_prices to another file.
    I've added mrid frome the timeseries list as the spot for DE_LU both have 15min prices from EXAA and 60min prices from EPEX. I use the same value as the API.

--- R/en_transmission_day_ahead_prices.R
+++ R/en_transmission_day_ahead_prices.R
@@ -10,20 +10,23 @@
 #'
 #' @examples
 #'
-#' library(tidyverse)
-#' library(entsoeapi)
+#'  library(tidyverse)
+#'  library(entsoeapi)
 #'
-#' dk1 <- en_transmission_day_ahead_prices(eic = "10YDK-1--------W",
-#'                                         period_start = lubridate::ymd("2019-10-27", tz = "CET"),
-#'                                         period_end = lubridate::ymd_hm("2019-10-27 23:00", tz = "CET"))
-#' dk1 <- en_transmission_day_ahead_prices(eic = "10YDK-1--------W",
-#'                                         period_start = lubridate::ymd("2019-03-31", tz = "CET"),
-#'                                         period_end = lubridate::ymd_hm("2019-03-31 23:00", tz = "CET"))
+#'  en_transmission_day_ahead_prices(eic = "10YCZ-CEPS-----N", period_start = lubridate::ymd("2019-11-01", tz = "CET"), period_end = lubridate::ymd("2019-12-01", tz = "CET"))
+#'  en_transmission_day_ahead_prices(eic = "10YDK-1--------W", period_start = lubridate::ymd("2019-11-01", tz = "CET"), period_end = lubridate::ymd("2019-12-01", tz = "CET"))
 #'
-en_transmission_day_ahead_prices <- function(eic, period_start, period_end, security_token = NULL){
+en_transmission_day_ahead_prices <- function(eic, period_start, period_end, in_domain = NULL, out_domain = NULL, security_token = NULL){
 
   period_start <- url_posixct_format(period_start)
   period_end <- url_posixct_format(period_end)
+
+  if(is.null(in_domain)){
+    in_domain <- eic
+  }
+  if(is.null(out_domain)){
+    out_domain <- eic
+  }
 
   if(is.null(security_token)){
     security_token <- Sys.getenv("ENTSOE_PAT")
@@ -36,8 +39,8 @@ en_transmission_day_ahead_prices <- function(eic, period_start, period_end, secu
   url <- paste0(
     "https://transparency.entsoe.eu/api",
     "?documentType=A44",
-    "&in_Domain=", eic,
-    "&out_Domain=", eic,
+    "&in_Domain=", in_domain,
+    "&out_Domain=", out_domain,
     "&periodStart=",period_start,
     "&periodEnd=", period_end,
     "&securityToken=", security_token
@@ -47,30 +50,20 @@ en_transmission_day_ahead_prices <- function(eic, period_start, period_end, secu
 
   en_cont <- xml2::as_list(en_cont)
 
-  en_cont <- tm_day_ahead_helper(en_cont)
+  en_currency <- en_cont$Publication_MarketDocument$TimeSeries$currency_Unit.name[[1]]
+  en_price_unit <- en_cont$Publication_MarketDocument$TimeSeries$price_Measure_Unit.name[[1]]
+
+  en_cont <- tm_price_helper(en_cont)
+
+  en_cont$price <- as.numeric(en_cont$price)
+  en_cont$currency_unit <- en_currency
+  en_cont$price_unit <- en_price_unit
+  en_cont$in_domain_mrid <- in_domain
+  en_cont$out_domain_mrid <- out_domain
+
+  en_cont <- en_cont[, c("in_domain_mrid", "out_domain_mrid", "currency_unit", "price_unit", "dt", "price", "mrid")]
 
   en_cont
 }
 
-
-tm_day_ahead_helper <- function(x){
-
-  x <- x$Publication_MarketDocument[names(x$Publication_MarketDocument) == "TimeSeries"]
-
-  x <- purrr::map(x, ~timeseries_extract_day_ahead(.x))
-  x <- dplyr::bind_rows(x)
-
-  x
-}
-
-timeseries_extract_day_ahead <- function(x){
-  dt <- lubridate::ymd_hm(x$Period$timeInterval$start[[1]], tz = "UTC")
-  position <- as.integer(unlist(purrr::map(x$Period, "position")))
-  price <- unlist(purrr::map(x$Period, "price.amount"))
-
-  dt <- dt_helper(tz_start = dt, tz_resolution = x$Period$resolution[[1]], tz_position = position)
-
-  tibble::tibble(dt, price)
-
-}
 
