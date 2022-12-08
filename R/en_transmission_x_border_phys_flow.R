23459b61a394cbe71d80cb186a45c4ed1f32dd71
Sándor Budai <sbudai.ga@gmail.com>
2022-12-08 19:27:36 +0100

    made it more error prone

--- R/en_transmission_x_border_phys_flow.R
+++ R/en_transmission_x_border_phys_flow.R
@@ -17,14 +17,10 @@
 #'  en_transmission_x_border_phys_flow(eic_in = "10Y1001A1001A83F", eic_out = "10YCZ-CEPS-----N", period_start = lubridate::ymd("2020-01-01", tz = "CET"), period_end = lubridate::ymd("2020-01-02", tz = "CET"))
 #'  en_transmission_x_border_phys_flow(eic_in = "10YCZ-CEPS-----N", eic_out = "10Y1001A1001A83F", period_start = lubridate::ymd("2020-01-01", tz = "CET"), period_end = lubridate::ymd("2020-01-02", tz = "CET"))
 #'
-en_transmission_x_border_phys_flow <- function(eic_in, eic_out, period_start, period_end, security_token = NULL){
+en_transmission_x_border_phys_flow <- function(eic_in, eic_out, period_start, period_end, security_token = Sys.getenv("ENTSOE_PAT")){
 
   period_start <- url_posixct_format(period_start)
   period_end <- url_posixct_format(period_end)
-
-  if(is.null(security_token)){
-    security_token <- Sys.getenv("ENTSOE_PAT")
-  }
 
   if(length(eic_in) > 1 | length(eic_out) > 1){
     stop("This wrapper only supports one EIC per request.")
@@ -43,7 +39,6 @@ en_transmission_x_border_phys_flow <- function(eic_in, eic_out, period_start, pe
   en_cont <- api_req(url)
   en_cont <- xml2::as_list(en_cont)
   en_measure_unit <- en_cont$Publication_MarketDocument$TimeSeries$quantity_Measure_Unit.name[[1]]
-
   en_cont <- tm_quantity_helper(en_cont, "quantity")
   colnames(en_cont)[names(en_cont) == "value"] <- "quantity"
 
@@ -51,7 +46,8 @@ en_transmission_x_border_phys_flow <- function(eic_in, eic_out, period_start, pe
   en_cont$in_domain_mrid <- eic_in
   en_cont$out_domain_mrid <- eic_out
 
-  en_cont <- en_cont[, c("in_domain_mrid", "out_domain_mrid", "measure_unit", "dt", "quantity")]
+  en_cont <- en_cont %>%
+    dplyr::select(tidyselect::any_of(c("in_domain_mrid", "out_domain_mrid", "measure_unit", "dt", "quantity", "resolution")))
 
   en_cont
 }
