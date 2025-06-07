203e7333e85b0525271c9c37ebe580b1d65e500c
sbudai <sbudai.ga@gmail.com>
2025-06-08 00:24:49 +0200

    CODE >> Code

--- data-raw/energy_product_types.R
+++ data-raw/energy_product_types.R
@@ -3,7 +3,7 @@ energy_product_types <- fs::path("data-raw",
                                  "energy_product_types",
                                  ext = "tsv") |>
   data.table::fread(encoding = "UTF-8") |>
-  dplyr::mutate(CODE = as.character(CODE)) |>
+  dplyr::mutate(Code = as.character(Code)) |>
   purrr::modify_if(is.character, trimws, which = "both") |>
   purrr::discard(~is.na(.x) |> all())
 
