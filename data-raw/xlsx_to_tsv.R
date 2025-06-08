442c0d8db63687089aa74b4cc806a2751e0023a4
sbudai <sbudai.ga@gmail.com>
2025-06-08 13:49:22 +0200

    linted

--- data-raw/xlsx_to_tsv.R
+++ data-raw/xlsx_to_tsv.R
@@ -5,9 +5,17 @@ purrr::walk2(
   names(df_list),
   function(df, sheet_name) {
     if (endsWith(x = sheet_name, suffix = "Type")) {
-      file_path <- paste0("data-raw/", snakecase::to_snake_case(sheet_name), "s.tsv")
+      file_path <- paste0(
+        "data-raw/",
+        snakecase::to_snake_case(sheet_name),
+        "s.tsv"
+        )
     } else {
-      file_path <- paste0("data-raw/", snakecase::to_snake_case(sheet_name), ".tsv")
+      file_path <- paste0(
+        "data-raw/",
+        snakecase::to_snake_case(sheet_name),
+        ".tsv"
+        )
     }
     dt <- purrr::map(df, gsub, pattern = "\\n", replacement = "") |>
       data.table::as.data.table()
